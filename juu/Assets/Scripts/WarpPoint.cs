adda2e8cd1a600478e2bfbb02ddf8e59aad14d2e
sota <sota.tubo@gmail.com>
2018-11-24 16:42:00 +0900

    ゲートとワープポイント作成

---
+++ juu/Assets/Scripts/WarpPoint.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WarpPoint : MonoBehaviour {
+
+	private GameObject warpTarget;
+
+	// Use this for initialization
+	void Start () {
+		warpTarget = GameObject.Find("WarpStart");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	private void OnTriggerEnter(Collider other)
+	{
+		if (other.gameObject.tag == "Player")
+		{
+			other.gameObject.transform.position = warpTarget.transform.position;
+		}
+	}
+
+}
