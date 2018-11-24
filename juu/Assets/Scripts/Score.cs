f71b9bef7f7a5d2a13978db5798f1065a018f48a
sota <sota.tubo@gmail.com>
2018-11-25 04:21:48 +0900

    スコア設定(未完成)など

---
+++ juu/Assets/Scripts/Score.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Score : MonoBehaviour {
+
+	public static int score;
+	private float timeScore;
+	[SerializeField]
+	private Timer timer;
+	[SerializeField]
+	private WarpPoint warpPoint;
+
+	// Use this for initialization
+	void Start () {
+		score = 0;
+		timeScore = -1;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	private void FixedUpdate()
+	{
+		if (warpPoint.scoreplus == true)
+		{
+			warpPoint.scoreplus = false;
+			timeScore = timer.timesec;
+			plus(timeScore);
+		}
+	}
+
+    void plus(float time)
+	{
+		//スコアの配分
+	}
+
+}
--- juu/Assets/Scripts/WarpPoint.cs
+++ juu/Assets/Scripts/WarpPoint.cs
@@ -5,21 +5,24 @@ using UnityEngine;
 public class WarpPoint : MonoBehaviour {
 
 	private GameObject warpTarget;
+	public bool scoreplus { get; set; }
 
 	// Use this for initialization
 	void Start () {
 		warpTarget = GameObject.Find("WarpStart");
+		scoreplus = false;
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		
 	}
-
+    
 	private void OnTriggerEnter(Collider other)
 	{
-		if (other.gameObject.tag == "Player")
+		if (other.gameObject.tag == "Player" && scoreplus == false)
 		{
+			scoreplus = true;
 			other.gameObject.transform.position = warpTarget.transform.position;
 		}
 	}
